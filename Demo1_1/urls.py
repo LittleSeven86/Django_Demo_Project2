2fea3ad5a4af642fdc4f5054039ec3141acf31d6
张猛 <kailzhang893@outlook.com>
2023-01-01 13:38:15 +0800

    更新最新路由备注

--- Demo1_1/urls.py
+++ Demo1_1/urls.py
@@ -15,9 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from projects.views import index
+from projects import views
+
+'''
+一、什么是路由？
+    1、定义
+        指的是url和后端视图之间的一一映射关系
+        
+    2、添加
+        需要在全局路由文件中（urls.py），urlpatterns列表中添加路由条目
+        urlpatterns列表条目数就是路由总数
+        urlpatterns列表从上到下进行匹配（路由寻址）
+        urlpatterns列表中条目一旦匹配成功，就会终止向下匹配，不成功就会一直向下匹配，全部不成功会抛出404
+        
+    3、path函数
+        用于定义路由条目
+        第一个参数位URL路径参数，为String，路径前不能添加/，路径最后需要添加/
+        
+        
+'''
+
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('index/',index),
+    path('index/',views.index),
+    path('get_projects/',views.get_projects),
+    path('get_projects/1',views.get_projects1),
+    path('get_projects/2',views.get_projects2)
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+#在views
 # Create your views here.
 def index(request):
-
     return HttpResponse("欢迎测试开发的大佬们")
+
+def get_projects(request):
+    return HttpResponse("<h1>这是一个项目信息</h1>")
+
+def get_projects1(request):
+    return HttpResponse("<h1>这是一个项目信息1</h1>")
+
+def get_projects2(request):
+    return HttpResponse("<h1>这是一个项目信息2</h1>")
