420e2693ba6bf0d7f869a27ac51bed50d1ef7fd0
张猛 <kailzhang893@outlook.com>
2023-02-07 21:47:25 +0800

    2023 0207 更新DRF框架 序列化器和反序列化器，一些字段的学习

--- projects/serializers.py
+++ projects/serializers.py
@@ -17,8 +17,40 @@ from rest_framework import serializers
 
 class ProjectSerializer(serializers.Serializer):
     '''
-
+    二、定义序列化器类
+        1.必须得继承Serializer类或者Serializer子类
+        2.定义的序列化器类中，字段名要与模型类中的字段名保持一致
+        3.定义的序列化器类的字段（类属性）为Field子类
+        4.默认定义哪些字段，那么哪些字段就会返回前端，同时也必须的输入（前端需要传递）
+        5.常用的序列化器字段类型
+            IntegerField  -> int
+            CharField     -> str
+            BooleanField  -> bool
+            DateTimeField -> datetime
+        6.可以在序列化器字段中指定不同的选项
+            label和help_text，与模型类中的verbose_name和help_text参数一样
+            IntegerField，可以使用max_value指定最大值，min_value指定最小值
+            CharField，可以使用max_length指定最大长度，min_length指定最小长度
+        7.定义的序列化器字段中，required默认为True，说明默认定义的字段必须得输入和输出
+        8.如果在序列化器字段中，设置required=False，那么前端用户可以不传递该字段（校验时会忽略改该字段，所以不会报错）
+        9.如果未定义模型类中的某个字段，那么该字段不会输入，也不会输出
+        10.前端必须的输入（反序列化输入）name（必须得校验），但是不会需要输出（序列化器输出）？
+            如果某个参数指定了write_only=True，那么该字段仅仅只输入（反序列化输入，做数据校验），不会输出（序列化器输出），
+            默认write_only为False
+        11.前端可以不用传递，但是后端需要输出？
+            如果某个参数指定了read_only=True，那么该字段仅仅只输出（序列化器输出），不会输入（反序列化输入，做数据校验），
+            默认read_only为False
+        12.在序列化器类中定义的字段，默认allow_null=False，该字段不允许传递null空值，
+            如果指定allow_null=True，那么该字段允许传递null
+        13.在序列化器类中定义CharField字段，默认allow_blank=False，该字段不允许传递空字符串，
+            如果指定allow_blank=True，那么该字段允许传递空字符串
+        14.在序列化器类中定义的字段，可以使用default参数来指定默认值，如果指定了default参数，那么前端用户可以不用传递，
+            会将default指定的值作为入参
     '''
-    id = serializers.IntegerField()
-    name = serializers.CharField()
-    leader = serializers.CharField()
+    # id = serializers.IntegerField(label='项目id', help_text='项目id', max_value=1000, min_value=1)
+    # name = serializers.CharField(label='项目名称', help_text='项目名称', max_length=20, min_length=5, write_only=True)
+    # name = serializers.CharField(label='项目名称', help_text='项目名称', max_length=20, min_length=5, read_only=True)
+    name = serializers.CharField(label='项目名称', help_text='项目名称', max_length=20, min_length=5)
+    # leader = serializers.CharField(label='项目负责人', help_text='项目负责人', allow_null=True)
+    # leader = serializers.CharField(label='项目负责人', help_text='项目负责人', allow_blank=True)
+    leader = serializers.CharField(label='项目负责人', help_text='项目负责人', default='阿名')
--- projects/views.py
+++ projects/views.py
@@ -10,9 +10,6 @@ from projects.serializers import ProjectSerializer
 # Create your views here.
 
 
-
-
-
 '''
     需求：
     1、获取一条项目数据
@@ -29,6 +26,7 @@ from projects.serializers import ProjectSerializer
 
 
 class ProjectsView(View):
+
     def get(self, request):
         '''
         2、获取所有项目数据
@@ -56,7 +54,16 @@ class ProjectsView(View):
         #         'name': item.name,
         #         'leader': item.leader
         #     })
-        serializer = ProjectSerializer(instance=queryset,many=True)
+        '''
+        三、序列化器的使用
+            1.可以使用序列化器进行序列化输出操作
+            a.创建序列化器对象
+            b.可以将模型对象或者查询集对象、普通对象、嵌套普通对象的列表，以instance关键字来传递参数
+            c.如果传递的是查询集对象、嵌套普通对象的列表（多条数据），必须得设置many=True
+            d.如果传递的是模型对象、普通对象，不需要设置many=True
+            e.可以使用序列化器对象的.data属性，获取序列化器之后的数据（字典、嵌套字典的列表）
+            '''
+        serializer = ProjectSerializer(instance=queryset, many=True)
         # 列表推导式
         # project_list = [{
         #         'id': item.id,
@@ -65,7 +72,7 @@ class ProjectsView(View):
         #     } for item in queryset]
 
         # return JsonResponse(project_list, safe=False)
-        return JsonResponse(serializer.data,safe=False)
+        return JsonResponse(serializer.data, safe=False)
 
     def post(self, request):
         '''
@@ -86,20 +93,29 @@ class ProjectsView(View):
         except Exception as e:
             return JsonResponse({'msg': '参数有误'}, status=400)
 
+        '''
+        四、反序列化操作
+        1.定义序列化器类，使用data关键字参数传递字典参数
+        2.可以使用序列化器对象调用.is_valid()方法，才会开始对前端输入的参数进行校验
+        3.如果校验通过.is_valid()方法返回True，否则返回False
+        4.如果调用.is_valid()方法，添加raise_exeception=True，校验不通过会抛出异常，否则不会抛出异常
+        5.只有在调用.is_valid()方法之后，才可以使用序列化器对象调用.errors属性，来获取错误提示信息（字典类型）
+        6.只有在调用.is_valid()方法之后，才可以使用序列化器对象调用.validated_data属性，来获取校验通过之后的数据，与使用json.load转化之后的数据有区别
+        '''
+        serializer11 = ProjectSerializer(data=python_data)
+        if not serializer11.is_valid():
+            return JsonResponse(serializer11.errors,status=401)
+
         # 3、创建数据
         # project_obj = Projects.objects.create(name=python_data.get('name'),
         #                                       leader=python_data.get('leader'),
         #                                       is_execute=python_data.get('is_execute'),
         #                                       desc=python_data.get('desc'))
-        project_obj = Projects.objects.create(**python_data)
+        # project_obj = Projects.objects.create(**python_data)
+        project_obj = Projects.objects.create(**serializer11.validated_data)
 
         # 4、将创建成功的数据返回给前端
-        python_dict = {
-            'id': project_obj.id,
-            'name': project_obj.name,
-            'msg': '创建成功'
-        }
-        return JsonResponse(python_dict, status=201)
+        return JsonResponse(serializer11.data, status=201)
 
 
 class ProjectsDetailView(View):
@@ -151,23 +167,22 @@ class ProjectsDetailView(View):
             return JsonResponse({'msg': '参数有误'}, status=400)
 
         # 需要进行大量的数据校验，且非常复杂
+        serializer11 = ProjectSerializer(data=python_data)
+        if not serializer11.is_valid():
+            return JsonResponse(serializer11.errors, status=401)
+
         # 4、更新数据
-        project_obj.name = python_data.get('name')
-        project_obj.is_excute = python_data.get('is_excute')
-        project_obj.leader = python_data.get('leader')
-        project_obj.desc = python_data.get('desc')
+        project_obj.name = serializer11.validated_data.get('name')
+        project_obj.is_excute = serializer11.validated_data.get('is_excute')
+        project_obj.leader = serializer11.validated_data.get('leader')
+        project_obj.desc = serializer11.validated_data.get('desc')
         project_obj.save()
 
         # 5、将读取的项目数据转化为字典
-        python_dict = {
-            'id':project_obj.id,
-            'name':project_obj.name,
-            'msg':'更新项目成功'
-        }
+        serializer = ProjectSerializer(instance=project_obj)
+        return JsonResponse(serializer.data, status=201)
 
-        return JsonResponse(python_dict,status=201)
-
-    def delete(self,request,pk):
+    def delete(self, request, pk):
         '''
         1、需要校验pk在数据库中是否存在
         2、读取主键为pk的项目数据
@@ -180,4 +195,3 @@ class ProjectsDetailView(View):
         # 3、执行删除
         project_obj.delete()
         return JsonResponse({'msg': '删除成功'}, status=204)
-
